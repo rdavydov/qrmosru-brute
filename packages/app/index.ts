2ab5590eea4f4c98a6b17a605ee2df1caa83bc64
ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ <w3bsmes@gmail.com>
2021-08-05 11:05:57 +0300

    Minor fixes and improvements

---
+++ packages/app/index.ts
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+import puppeteer from "puppeteer";
+
+type Count = number;
+
+/**
+ * Count of SS
+ *
+ * @var {Number}
+ */
+const count: Count = 100;
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  for (let i = 0; i < count; i++) {
+    await page.goto(
+      `https://www.gosuslugi.ru/vaccine/cert/verify/${uuidv4()}`,
+      { waitUntil: "networkidle2" }
+    );
+    await page.screenshot({
+      path: `${__dirname}/${uuidv4()}.jpeg`,
+    });
+  }
+  await browser.close();
+})();
